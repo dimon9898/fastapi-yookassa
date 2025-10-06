41167d7b33887c934f7acbd7ffea1db3209de1a3
MacDimon <macdimon@MacBook-Air-MacDimon.local>
2025-10-07 01:51:11 +0500

    logger99

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from aiogram import Bot
 from fastapi import FastAPI, HTTPException, Request, status, Depends
@@ -23,6 +24,22 @@ app = FastAPI()
 @app.post('/webhook', status_code=status.HTTP_200_OK)
 async def payment_webhook(request: Request, bot: Bot = Depends(get_bot)):
     data = await request.json()
+    
+    if data.get('event') == 'payment.succeeded':
+        payment = data.get('object')
+        metadata = payment.get('metadata')
+        
+        user_id = metadata.get('user_id')
+        
+        if user_id:
+            asyncio.create_task(send_notification(user_id, text='ВСЕ ОКЕЙ'))
+    
+    
     return {'ok': True}
+
+
+
+async def send_notification(user_id, text):
+    await bot.send_message(chat_id=user_id, text=text)
 
 
