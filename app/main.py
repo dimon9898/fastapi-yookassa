323c3fccb0980d51ea3c333293fb2f8a80037f4c
MacDimon <macdimon@MacBook-Air-MacDimon.local>
2025-10-07 01:13:14 +0500

    logger3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import json
 from aiogram import Bot
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request, status, Depends
 import logging
 from run import bot
 
@@ -13,14 +13,15 @@ logging.basicConfig(
 
 logger = logging.getLogger('fastapi')
 
-
+async def get_bot():
+    return bot
 
 
 app = FastAPI()
 
 
 @app.post('/webhook')
-async def payment_webhook(request: Request, bot: Bot):
+async def payment_webhook(request: Request, bot: Bot = Depends(get_bot)):
     data = await request.json()
     result = json.dumps(data, indent=4, ensure_ascii=False)
     await bot.send_message(chat_id=7886074197, text=f'{result}')
